2079f75b8422296faeda62b0d53673a9c64bc0d2
paratoxicdev <paratoxic@parasite.dev>
2025-07-31 21:56:37 -0400

    Use aggregator (#69)

--- app/api/user/[address]/route.ts
+++ app/api/user/[address]/route.ts
@@ -56,7 +56,7 @@ export async function GET(
     const { address } = await params;
     
     // Fetch user data from parasite.wtf
-    const response = await fetch(`https://parasite.wtf/users/${address}`, {
+    const response = await fetch(`https://alpha.parasite.dev/aggregator/users/${address}`, {
       next: { revalidate: 10 } // Cache for 10 seconds
     });
     
