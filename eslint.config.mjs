43ca322d85899cde265dba7644ec1ba3441c6fb9
paratoxic <paratoxic@parasite.dev>
2025-12-08 20:16:27 -0500

    Relevel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,6 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "react-hooks/set-state-in-effect": "warn",
-      "react-hooks/immutability": "warn"
     }
   }
 ];
