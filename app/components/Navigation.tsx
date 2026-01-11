80800a1f2562f62b4366ded81f7a9d0274e68447
Celshade <ggcelshade@gmail.com>
2026-01-11 12:15:55 -0500

    Fix Mobile Display and Missing Nav (#98)

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -52,15 +52,15 @@ export default function Navigation({
   }, []);
 
   return (
-    <header className={`bg-background text-foreground py-4`}>
-      <div className="container mx-auto flex items-end gap-4">
+    <header className={`bg-background text-foreground py-4 relative`}>
+      <div className="container mx-auto flex flex-col md:flex-row md:items-end gap-4">
         {/* Left: Logo */}
-        <Link href="/" className="hidden sm:flex flex-shrink-0 w-[200px] lg:w-[300px] justify-start">
+        <Link href="/" className="flex flex-shrink-0 w-[200px] lg:w-[300px] justify-start">
           <Image src={parasiteLogo} alt="Parasite Logo" height={64} />
         </Link>
 
-        {/* Center: Search input or user/worker name */}
-        <div className="flex-grow flex justify-center">
+        {/* Center: Search input */}
+        <div className="flex-grow flex justify-center order-last md:order-none">
             <div className="relative w-full max-w-md">
               <input
                 ref={inputRef}
@@ -71,7 +71,7 @@ export default function Navigation({
                 placeholder="Enter your wallet address... (Ctrl/Cmd + K)"
                 className="w-full py-2 px-2 xl:px-4 bg-background border border-gray-300 focus:outline-none focus:ring-2 focus:ring-foreground text-xs xl:text-sm"
               />
-              <button 
+              <button
                 onClick={() => onAddressSubmit(address)}
                 className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-background text-gray-400 hover:text-gray-600"
                 aria-label="Search"
@@ -84,11 +84,8 @@ export default function Navigation({
         </div>
 
         {/* Right: Connect Button */}
-        <div className="flex-shrink-0 flex items-center space-x-4 w-auto sm:w-[200px] lg:w-[300px] justify-end">
+        <div className="flex-shrink-0 flex items-center space-x-4 w-auto sm:w-[200px] lg:w-[300px] justify-end absolute right-4 top-4 md:relative md:right-auto md:top-auto">
           <ConnectButton />
-          {/* <Link href="/" className="hidden sm:block">
-            <Image src={parasiteBug} alt="Parasite Bug" height={64} className='h-10 sm:h-12 w-auto' />
-          </Link> */}
         </div>
 
         {/* Right: Pool status */}
@@ -111,4 +108,4 @@ export default function Navigation({
       />
     </header>
   );
-} 
+}
