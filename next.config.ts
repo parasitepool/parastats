7558d2bd0e1e03a1ff69368ed30d2c44158d7230
paratoxicdev <paratoxic@parasite.dev>
2026-03-20 16:30:06 -0400

    Fix some miscellaneous warnings (#200)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingExcludes: {
+    '*': ['./next.config.ts'],
+  },
   images: {
     remotePatterns: [
       {
